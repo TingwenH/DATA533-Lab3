78ff145f8db12116f30ad6dd2257978b2dc3802c
Saisree-123 <saisree.gr@gmail.com>
2021-12-01 21:11:25 -0800

    final testing

--- package/budget_subpackage/test_budget_subpackge.py
+++ package/budget_subpackage/test_budget_subpackge.py
@@ -1,22 +1,23 @@
 from freemembership import FreeMemberShip
 from basicmembership import BasicMembership
 from premiummembership import PremiumMembership
+#from ..user_subpackage.personal_info import personal_info
 
 def testing_budget():
+    #person1 = personal_info("Fanny",30,"1234@gmail.com",120000)
     #Free membership
     free_mem1=FreeMemberShip(25000)
     free_mem1.add_amount(1500)
 
     #Basic membership
     basic_mem1=BasicMembership()
-    annual=basic_mem1.analysis_and_suggestion()
+    annual=basic_mem1.analysis_and_suggestion(10000)
     free_mem2 =FreeMemberShip(annual)
     free_mem2.add_amount(30)
 
     #Premium membership
     premium_mem1=PremiumMembership()
-    premium_mem1.expenditure_chart()
-    annual=premium_mem1.analysis_and_suggestion()
+    annual=premium_mem1.analysis_and_suggestion(10000)
     free_mem3=FreeMemberShip(annual)
     free_mem3.add_amount(500)
     
